140c709c2b579f030cbef5606454ebd23c896aaa
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-01-30 21:59:12 -0200

    added failing test of auto-search feature

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -1,18 +1,25 @@
 describe('Search controller', function() {
     var $this;
     var $location;
+    var $timeout;
 
     beforeEach(function() {
         module('movieApp');
     });
 
-    beforeEach(inject(function(_$controller_, _$location_) {
+    beforeEach(inject(function(_$controller_, _$location_, _$timeout_) {
         $location = _$location_;
+        $timeout = _$timeout_;
 
         $this = _$controller_('SearchController', {
-            $location: $location
+            $location: $location,
+            $timeout: $timeout
         });
     }));
+
+    afterEach(function() {
+        $timeout.verifyNoPendingTasks();
+    });
 
     it('should redirect to the query results page for non-empty query', function() {
         $this.query = 'star wars';
@@ -24,5 +31,12 @@ describe('Search controller', function() {
         $this.query = '';
         $this.search();
         expect($location.url()).toBe('');
+    });
+
+    it('should be redirected to the results page after one second after the key press', function() {
+        $this.query = 'star wars';
+        $this.searchInputChanged();
+        $timeout.flush();
+        expect($location.url()).toBe('/results?q=star%20wars');
     });
 });
