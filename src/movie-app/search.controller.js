816aff6be508f2a6989cf6d9f73599f6cf192df1
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-01-30 22:24:50 -0200

    implement search on inactivity feature

--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -3,15 +3,26 @@
     angular.module('movieApp')
         .controller('SearchController', SearchController);
 
-    SearchController.$inject = ['$location'];
+    SearchController.$inject = ['$location', '$timeout'];
 
-    function SearchController($location) {
+    function SearchController($location, $timeout) {
         var vm = this;
+        var autoSearchTimeout;
+
+        var AUTO_SEARCH_AFTER_MS = 1000;
+
         vm.search = search;
+        vm.searchInputChanged = searchInputChanged;
 
         function search() {
+            $timeout.cancel(autoSearchTimeout);
             if (!vm.query) return;
             $location.path('/results').search('q', vm.query);
+        }
+
+        function searchInputChanged() {
+            $timeout.cancel(autoSearchTimeout);
+            autoSearchTimeout = $timeout(vm.search, AUTO_SEARCH_AFTER_MS);
         }
     }
 }());
