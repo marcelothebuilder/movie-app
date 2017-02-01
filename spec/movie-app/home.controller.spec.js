df70b261b3f99517ac27821626438ec77ff3ffb5
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-01-31 23:28:41 -0200

    add rotating popular movies feature

--- spec/movie-app/home.controller.spec.js
+++ spec/movie-app/home.controller.spec.js
@@ -1,59 +1,60 @@
 describe('Home controller', function() {
     var $this;
-    var $interval;
-    var PopularMovies;
 
     var CHANGE_MOVIE_INTERVAL_MS = 5000;
 
     beforeEach(function() {
         module('movieApp');
         module('movieCore');
+        module('omdb');
     });
 
-    beforeEach(inject(function(_$controller_, _$interval_, _PopularMovies_) {
-        PopularMovies = _PopularMovies_;
-        $interval = _$interval_;
+    beforeEach(inject(function(_$q_, _PopularMovies_, _OmdbApi_) {
+        spyOn(_PopularMovies_, 'get').and.callFake(function() {
+            return _$q_.resolve([
+                'tt0076751',
+                'tt0076752',
+                'tt0076753'
+            ]);
+        });
 
-        $this = _$controller_('HomeController', {
-            $interval: $interval,
-            PopularMovies: PopularMovies
+        spyOn(_OmdbApi_, 'findById').and.callFake(function(id) {
+            return _$q_.resolve({
+                imdbID: id
+            });
         });
     }));
 
-    beforeEach(function() {
-        spyOn(PopularMovies, 'query').and.callFake(function() {
-            return [{
-                    movieId: 'tt0076751',
-                    description: 'A great movie 1 :)'
-                },
-                {
-                    movieId: 'tt0076752',
-                    description: 'A great movie 2 :)'
-                },
-                {
-                    movieId: 'tt0076753',
-                    description: 'A great movie 3 :)'
-                }
-            ];
+    beforeEach(inject(function(_$controller_, _$interval_, _PopularMovies_, _OmdbApi_, _$rootScope_) {
+        $this = _$controller_('HomeController', {
+            $interval: _$interval_,
+            PopularMovies: _PopularMovies_,
+            OmdbApi: _OmdbApi_
         });
-    });
+
+        _$rootScope_.$apply();
+    }));
+
+    beforeEach(inject(function(_$interval_) {
+        $interval = _$interval_;
+    }));
 
     it('should be at the first movie if interval called callback 0 times', function() {
-        expect($this.currentMovie.movieId).toEqual('tt0076751');
+        expect($this.currentMovie.imdbID).toEqual('tt0076751');
     });
 
     it('should be at the second movie if interval called callback 1 times', function() {
         $interval.flush(CHANGE_MOVIE_INTERVAL_MS);
-        expect($this.currentMovie.movieId).toEqual('tt0076752');
+        expect($this.currentMovie.imdbID).toEqual('tt0076752');
     });
 
     it('should be at the third movie if interval called callback 2 times', function() {
-        $interval.flush(CHANGE_MOVIE_INTERVAL_MS*2);
-        expect($this.currentMovie.movieId).toEqual('tt0076753');
+        $interval.flush(CHANGE_MOVIE_INTERVAL_MS * 2);
+        expect($this.currentMovie.imdbID).toEqual('tt0076753');
     });
 
     it('should be at the first movie again if interval called callback 3 times', function() {
-        $interval.flush(CHANGE_MOVIE_INTERVAL_MS*3);
-        expect($this.currentMovie.movieId).toEqual('tt0076751');
+        $interval.flush(CHANGE_MOVIE_INTERVAL_MS * 3);
+        expect($this.currentMovie.imdbID).toEqual('tt0076751');
     });
 });
---
+++ src/movie-app/home.controller.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+    angular.module('movieApp')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['PopularMovies', '$interval', 'OmdbApi'];
+
+    function HomeController(popularMovies, $interval, omdbApi) {
+        var vm = this;
+        var interval;
+        var CHANGE_MOVIE_INTERVAL_MS = 5000;
+        var popularMoviesIds = [];
+        var currentMovieIndex = 0;
+
+        vm.currentMovie = {};
+        start();
+
+        function start() {
+            popularMovies.get()
+                .then(function(popularMoviesIds_) {
+                    popularMoviesIds = popularMoviesIds_;
+                    populateMovieData();
+                    startRotateInterval();
+                });
+        }
+
+        function cycleMovieIndex() {
+            if (popularMoviesIds.length === 0) return;
+            if (currentMovieIndex === (popularMoviesIds.length - 1)) {
+                currentMovieIndex = 0;
+            } else {
+                currentMovieIndex++;
+            }
+        }
+
+        function startRotateInterval() {
+            $interval(function () {
+                cycleMovieIndex();
+                populateMovieData();
+            }, CHANGE_MOVIE_INTERVAL_MS);
+        }
+
+        function populateMovieData() {
+            var movieId = popularMoviesIds[currentMovieIndex];
+
+            return omdbApi.findById(movieId)
+                .then(function(movieData) {
+                    vm.currentMovie = movieData;
+                });
+        }
+    }
+}());
