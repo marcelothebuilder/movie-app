3a030454e9a8cf5656e37603a440af3cab4e970f
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-02-01 22:25:28 -0200

    refactor movie poster directive into component

--- src/movie-app/movie-info-poster.directive.js
+++ src/movie-app/movie-info-poster.directive.js
@@ -1,19 +1,10 @@
 (function() {
-  'use strict';
-  angular.module('movieApp')
-      .directive('movieInfoPoster', movieInfoPoster);
-
-  movieInfoPoster.$inject = [];
-
-  function movieInfoPoster() {
-      return {
-          restrict: 'E',
-          templateUrl: 'movie-app/movie-info-poster.directive.html',
-          // template: '<div>{{info.Title}}</div>',
-          replace: false,
-          scope: {
-              info: '='
-          }
-      };
-  }
+    'use strict';
+    angular.module('movieApp')
+        .component('movieInfoPoster', {
+            templateUrl: 'movie-app/movie-info-poster.directive.html',
+            bindings: {
+                info: '='
+            }
+        });
 }());
