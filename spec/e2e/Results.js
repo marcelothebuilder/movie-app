2200833994540b976c288f8d82edece5b213b725
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-02-06 21:26:45 -0200

    test auto-search

--- spec/e2e/Results.js
+++ spec/e2e/Results.js
@@ -7,24 +7,35 @@ describe('Results page', function() {
                 browser.waitForAngular();
 
                 element(by.model('vm.query')).sendKeys('Star');
-
-                element(by.css('.btn.btn-default')).click();
-
-                browser.waitForAngular();
             });
 
-            it('should redirect to results', function() {
-                expect(browser.getCurrentUrl()).toMatch('result');
+            describe('and clicking the Go button', function() {
+                beforeEach(function() {
+                    element(by.buttonText('Go!')).click();
+
+                    browser.waitForAngular();
+                });
+
+                it('should redirect to results', function() {
+                    expect(browser.getCurrentUrl()).toMatch('result');
+                });
+
+                it('should show 10 results', function() {
+                    var results = element.all(by.repeater('result in vm.results'));
+                    expect(results.count()).toBe(10);
+                });
+
+                it('should show a header with a title about the first movie', function() {
+                    var firstMovie = element.all(by.repeater('result in vm.results')).first();
+                    expect(firstMovie.element(by.css('[uib-accordion-header]')).getText()).toMatch('Star Wars');
+                });
             });
 
-            it('should show 10 results', function() {
-                var results = element.all(by.repeater('result in vm.results'));
-                expect(results.count()).toBe(10);
-            });
-
-            it('should show a header with a title about the first movie', function () {
-                var firstMovie = element.all(by.repeater('result in vm.results')).first();
-                expect(firstMovie.element(by.css('[uib-accordion-header]')).getText()).toMatch('Star Wars');
+            describe("and waiting a few seconds", function() {
+                it('should automatically redirect to the results', function() {
+                    browser.sleep(3000);
+                    expect(browser.getCurrentUrl()).toMatch(/results\?q=Star/);
+                });
             });
         });
     });
