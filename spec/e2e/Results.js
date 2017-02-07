7257ee40c5aa3685b95be5f12245453d1b865866
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-02-06 22:57:06 -0200

    refactor test using pageobject pattern

--- spec/e2e/Results.js
+++ spec/e2e/Results.js
@@ -1,25 +1,53 @@
+var Search = function() {
+    var self = this;
+    self.get = function() {
+        browser.get('/');
+
+        browser.waitForAngular();
+    };
+
+    self.getSubmitButton = function() {
+        return element(by.buttonText('Go!'));
+    };
+
+    self.submit = function() {
+        self.getSubmitButton().click();
+        browser.waitForAngular();
+    };
+
+    self.getResults = function() {
+        return element.all(by.repeater('result in vm.results'));
+    };
+
+    self.getSearchInput = function() {
+        return element(by.model('vm.query'));
+    };
+};
+
 describe('Results page', function() {
+    var page;
+
+    beforeEach(function() {
+        page = new Search();
+    });
+
     describe('at home page', function() {
         beforeEach(function() {
-            browser.get('/');
-
-            browser.waitForAngular();
+            page.get();
         });
 
         it('should keep the Go! button disabled if the search field is empty', function() {
-            expect(element(by.buttonText('Go!')).getAttribute('disabled')).toBeTruthy();
+            expect(page.getSubmitButton().getAttribute('disabled')).toBeTruthy();
         });
 
         describe('when searching for the word Star', function() {
             beforeEach(function() {
-                element(by.model('vm.query')).sendKeys('Star');
+                page.getSearchInput().sendKeys('Star');
             });
 
             describe('and clicking the Go button', function() {
                 beforeEach(function() {
-                    element(by.buttonText('Go!')).click();
-
-                    browser.waitForAngular();
+                    page.submit();
                 });
 
                 it('should redirect to results', function() {
@@ -27,12 +55,12 @@ describe('Results page', function() {
                 });
 
                 it('should show 10 results', function() {
-                    var results = element.all(by.repeater('result in vm.results'));
+                    var results = page.getResults();
                     expect(results.count()).toBe(10);
                 });
 
                 it('should show a header with a title about the first movie', function() {
-                    var firstMovie = element.all(by.repeater('result in vm.results')).first();
+                    var firstMovie = page.getResults().first();
                     expect(firstMovie.element(by.css('[uib-accordion-header]')).getText()).toMatch('Star Wars');
                 });
             });
