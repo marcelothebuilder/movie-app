0a4f230ab61069fb921ec3f3fd9ed020f9170d78
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-02-06 22:02:19 -0200

    added test suites

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,9 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec/e2e/**/*.js'],
+    suites: {
+      home: 'spec/e2e/Home-popularMovie.js',
+      search: 'spec/e2e/Results.js'
+    },
     onPrepare: function() {
         browser.driver.manage().window().setPosition(0, 0);
         browser.driver.manage().window().setSize(1280, 720);
